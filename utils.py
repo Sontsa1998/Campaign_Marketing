2aa0d0d01285fbd7d705df4c986be8675385a32e
Sontsa1998 <christlove700@gmail.com>
2025-04-28 08:30:44 +0200

    Update visualization with static and dynamic graph using plotly and matplotlib librairies

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
 
 def connection():
     engine = create_engine("mssql://Chris_Evan_Noah/Campaign_Marketing?driver=ODBC+DRIVER+17+FOR+SQL+SERVER")
@@ -12,3 +14,17 @@ def run_query(query):
     df = pd.read_sql_query(query, con)
     con.close()
     return df
+
+#Fonction de visualisation de nos données
+def data_viz(df, xdata, ydata, xlabel, ylabel, title):
+    df.plot(kind='bar', x=xdata, y=ydata, legend=None)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.tight_layout()
+    plt.show()
+
+#fonction de visualisation interactive avec seaborn
+def dynamic_Viz(df, xdata, ydata, title):
+    fig = px.box(df, x=xdata, y=ydata, color=xdata, hover_name=xdata, title=title )
+    fig.show()
--- visualization.py
+++ visualization.py
@@ -1,1 +1,13 @@
-import index
+from index import proportion_client_statusMatrimoniale
+from index import proportion_clients_byEducation
+from utils import data_viz
+from utils import dynamic_Viz
+
+#resultat requete 1 : proportion des clients par situation matrimoniale
+###data_viz(df=proportion_client_statusMatrimoniale(), xdata='marital_status', ydata='Population', xlabel='Marital Status', ylabel='Population', title='Proportion des clients par status matrimoniale')
+
+#requete 2 : proportion des clients par Education
+###data_viz(df=proportion_clients_byEducation(), xdata='education', ydata='Population', xlabel='Education', ylabel='Population', title='Proportion des clients par Education ')
+
+#requete 3 : proportion des clients par Education
+dynamic_Viz(df=proportion_clients_byEducation(), xdata='education', ydata='Population', title='Proportion des clients par Education ')
